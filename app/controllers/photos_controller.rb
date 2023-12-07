aa45addeacdc566dc87fac08eae3626c62914fc0
tferrar0 <146215589+tferrar0@users.noreply.github.com>
2023-12-07 04:26:04 +0000

    comments in

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,7 +3,17 @@ class PhotosController < ApplicationController
     matching_photos = Photo.all
 
     @list_of_photos = matching_photos.order({ :created_at => :desc})
-    
+
     render({ :template => "photo_templates/index"})
+  end
+
+  def show
+    url_id = params.fetch("path_id")
+    
+    matching_photos = Photo.where({ :id => url_id })
+
+    @the_photo = matching_photos.at(0)
+
+    render({ :template => "photo_templates/show"})
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   get("/users", { :controller => "users", :action => "index"})
   get("/users/:path_username", { :controller => "users", :action => "show"})
   get("/photos", { :controller => "photos", :action => "index"})
+  get("/photos/:path_id", { :controller => "photos", :action => "show"})
 end
