77a4eeccb17ae72f773430d2e62e740a9d846a90
tferrar0 <146215589+tferrar0@users.noreply.github.com>
2023-12-07 19:58:54 +0000

    update done

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -16,4 +16,51 @@ class PhotosController < ApplicationController
 
     render({ :template => "photo_templates/show"})
   end
+
+  def destroy
+    the_id = params.fetch("delete_id")
+
+    matching_photos = Photo.where({ :id => the_id})
+
+    the_photo = matching_photos.at(0)
+
+    the_photo.destroy
+
+    redirect_to("/photos")
+  end
+
+  def create
+    # Parameters: {"query_image"=>"a", "query_caption"=>"b", "query_owner_id"=>"c"}
+
+    input_image = params.fetch("query_image")
+    input_caption = params.fetch("query_caption")
+    input_owner_id = params.fetch("query_owner_id")
+
+    a_new_photo = Photo.new
+    a_new_photo.image = input_image
+    a_new_photo.caption = input_caption
+    a_new_photo.owner_id = input_owner_id
+
+    a_new_photo.save
+    
+    redirect_to("/photos/#{a_new_photo.id}")
+  end
+
+  def update
+    the_id = params.fetch("modify_id")
+
+    matching_photos = Photo.where({ :id => the_id})
+
+    the_photo = matching_photos.at(0)
+
+    input_image = params.fetch("query_image")
+    input_caption = params.fetch("query_caption")
+
+    the_photo.image = input_image
+    the_photo.caption = input_caption
+
+    the_photo.save
+
+    redirect_to("/photos/#{the_photo.id}")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   get("/users/:path_username", { :controller => "users", :action => "show"})
   get("/photos", { :controller => "photos", :action => "index"})
   get("/photos/:path_id", { :controller => "photos", :action => "show"})
+  get("/delete_photo/:delete_id", { :controller => "photos", :action => "destroy"})
+  post("/insert_photo", { :controller => "photos", :action => "create"})
+  post("/update_photo/:modify_id", { :controller => "photos", :action => "update"})
 end
