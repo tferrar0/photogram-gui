acea52b9a9e4897a05de44346edcff48b3119a01
tferrar0 <146215589+tferrar0@users.noreply.github.com>
2023-12-07 03:20:26 +0000

    user details looks good

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,17 @@ class UsersController < ApplicationController
 
     render({ :template => "user_templates/index"})
   end
+
+  def show
+    #parameter: {"path_username" => "anisa"}
+    url_username = params.fetch("path_username")
+    matching_usernames = User.where({ :username => url_username })
+    @the_user = matching_usernames.first
+
+    if @the_user == nil
+        redirect_to("/404")
+    else
+    render({ :template => "user_templates/show"})
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
 
   get("/users", { :controller => "users", :action => "index"})
-
+  get("/users/:path_username", { :controller => "users", :action => "show"})
 end
